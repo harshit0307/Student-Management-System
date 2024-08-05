cbb095248237543bf683735640c3da83b629fa8e
harshit0307 <63691792+harshit0307@users.noreply.github.com>
2024-08-06 00:48:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,55 +2,56 @@
 #include "loading.h"
 #include <string>
 #include <Windows.h>
-#include "adminFunction.h"
+#include "loginSignup.h"
 
 using namespace std;
 
 void passwordVerification(void);
-void mainMenu(void);
+void loginSignup(void);
 
 //Auth Before opening the main program  
 
 void passwordVerification()
 {
     system("cls");
+    loginSignup();
 
-    string password;
+    //string password;
 
-    cout << "\n\n\n\n\n\t\t\t\t\t";
-    cout << "\n\n\n\t\t\t\t\tEnter Password : ";
+    //cout << "\n\n\n\n\n\t\t\t\t\t";
+    //cout << "\n\n\n\t\t\t\t\tEnter Password : ";
 
-    do
-    {
-        cin >> password;
+    //do
+    //{
+    //    cin >> password;
 
-        if (password != "password")
-        {
-            system("cls");
-            cout << "\n\n\n\n\n\t\t"
-                 << "wrong password!, try again or type 'quit' / 'exit' to Quit The program.";
-            cout << "\n\n\n\t\t\t\t\tEnter Password : ";
-        }
+    //    if (password != "password")
+    //    {
+    //        system("cls");
+    //        cout << "\n\n\n\n\n\t\t"
+    //             << "wrong password!, try again or type 'quit' / 'exit' to Quit The program.";
+    //        cout << "\n\n\n\t\t\t\t\tEnter Password : ";
+    //    }
 
-        if (password == "Quit" || password == "quit" || password == "exit" || password == "Exit" || password == "QUIT" || password == "EXIT")
-        {
-            system("cls");
-            cout << "\t\t\n\n\n\n\n\n\n\t\t\t\t\tQuitting The Program";
+    //    if (password == "Quit" || password == "quit" || password == "exit" || password == "Exit" || password == "QUIT" || password == "EXIT")
+    //    {
+    //        system("cls");
+    //        cout << "\t\t\n\n\n\n\n\n\n\t\t\t\t\tQuitting The Program";
 
-            for (int i = 0; i < 4; i++)
-            {
+    //        for (int i = 0; i < 4; i++)
+    //        {
 
-                Sleep(1000); // 1 second pause;
-                cout << ".";
-            }
+    //            Sleep(1000); // 1 second pause;
+    //            cout << ".";
+    //        }
 
-            exit(0);
-        }
+    //        exit(0);
+    //    }
 
-    }
+    //}
 
-    while (password != "password");
-    mainMenu(); // main menu function is in the Header File adminFunction.h
+    //while (password != "password");
+    //mainMenu(); // main menu function is in the Header File adminFunction.h
 }
 
 int main()
